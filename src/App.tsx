c6c1c868088a5bc2273b1846192bce7b170e1075
CG_Pack <wkdnffla3@gmail.com>
2025-07-07 20:50:08 +0900

    update : all

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Link} from 'react-router-dom';
-import Login from './pages/login';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/Login';
 
 
 import './App.css'
@@ -15,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Link to="/login">로그인</Link>
+        
       </Routes>
     </Router>
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login(){
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [error, setError] = React.useState("");
+    
+    const mockUser = [
+      {
+        email:"test@test",
+        password:"1234"
+      }
+    ];
+    //useState는 비동기 함수라서 함수 실행후에 값이 업데이트가 되지 않을수 있다.
+    //그래서 useEffect를 통해 값이 바뀔때 실행되게 만든다!
+    useEffect(() => {
+    if (error) {
+      alert(error);
+    }
+    }, [error]);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError("");
+       
 
         try{
-            // 로그인 api call // 로그인 개발 시에는 로그인 api가 개발되지 않아 mockup API 서버로 호출
-            const response = await fetch('/mockup-api/login',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({email,password})
+            // 로그인 api call 
+            // 로그인 개발 시에는 로그인 api가 개발되지 않아
+            // mockup API 서버로 호출 하는 방법도 잇다!
+            // const response = await fetch('/mockup-api/login',{
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({email,password})
 
-            });
-            if(!response.ok){
-                throw new Error('로그인 실패');
+            // });
+            // if(!response.ok){
+            //     throw new Error('로그인 실패');
+            // }
+            // const data = await response.json();
+            // localStroage.setItem('token', data.token);
+
+            const user = mockUser.find((user)=>{
+                if(email === user.email && password === user.password)
+                  return user;
+            })
+            if(!user){
+              throw new Error("로그인 실패");
             }
-            const data = await response.json();
 
-            localStroage.setItem('token', data.token);
-
-            window.location.href = '/dashboard'; // 로그인 성공 후 랜딩페이지 설정
+            window.location.href = '/landingpage'; // 로그인 성공 후 랜딩페이지 설정
         } catch(err){
             setError('이메일 또는 비밀번호가 잘못되었습니다.');
+            // console.log(err);
+            // alert(err);
+            
         }
     }
 
@@ -58,7 +86,7 @@ function Login(){
                     placeholder="비밀번호를 입력하세요"
                 ></input>
             </div>
-            {error && <div style={{color: 'red'}}>{error}</div>}
+            {/* {error && <div style={{color: 'red'}}>{error}</div>} */}
             <button type="submit">로그인</button>
 
 
