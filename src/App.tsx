6f7df20285406174509eb911a1003883de6a3513
CG_Pack <wkdnffla3@gmail.com>
2025-07-07 20:50:08 +0900

    team1-23  update : all

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Link} from 'react-router-dom';
-import Login from './pages/login';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/Login';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
       <Routes>
         
         <Route path="/" element={<Login />} />
-        <Link to="/login">로그인</Link>
+        
       </Routes>
     </Router>
 
