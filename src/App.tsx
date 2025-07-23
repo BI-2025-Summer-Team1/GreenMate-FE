975dd3fca48fd9dc06cf61945b44800606b3db20
CG_Pack <wkdnffla3@gmail.com>
2025-07-23 13:50:01 +0900

    team1-23 리베이스 중에 뭔가 꼬인거 같아 다시 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-
-
-import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {Link} from 'react-router-dom';
 import Login from './pages/Login';
-
 import './App.css'
 
 function App() {
