a094f3f0b1b403536233e183fd1538c60e47c9df
dudcns02 <lbs2080@naver.com>
2025-07-29 18:51:11 +0900

    TEAM-38 실수 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import CommunityWritePage from "./pages/CommunityWritePage";
-import ActivityWritePage from "./pages/ActivityWritePage";
+// import ActivityWritePage from "./pages/ActivityWritePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/community/write" element={<CommunityWritePage />} />
       <Route path="/" element={<h1>홈입니다</h1>} />
-      <Route path="/activity/write" element={<ActivityWritePage />} />
+      {/* <Route path="/activity/write" element={<ActivityWritePage />} /> */}
     </Routes>
   );
 }
