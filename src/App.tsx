56e2328aaf234ddb13db87ffd208813f00f6a501
dudcns02 <lbs2080@naver.com>
2025-08-12 21:56:23 +0900

    App.tsx에 ROUTES 상수 적용하여 경로 관리 통일

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import CommunityWritePage from "./pages/CommunityWritePage";
 import ActivityWritePage from "./pages/ActivityWritePage";
+import { ROUTES } from "./constants/routes";
 
 function App() {
   return (
     <Routes>
-      <Route path="/community/write" element={<CommunityWritePage />} />
-      <Route path="/activity/write" element={<ActivityWritePage />} />
+      <Route path={ROUTES.COMMUNITY_WRITE} element={<CommunityWritePage />} />
+      <Route path={ROUTES.ACTIVITY_WRITE} element={<ActivityWritePage />} />
     </Routes>
   );
 }
---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+  COMMUNITY_WRITE: "/community/write",
+  ACTIVITY_WRITE: "/activity/write",
+};
